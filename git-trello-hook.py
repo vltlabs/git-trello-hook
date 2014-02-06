042e1f4c9f625d5dc3ad423252b95d55e162586f
Ajmal Afif <ajmalafif@gmail.com>
2014-02-06 21:01:12 +0800

    return done

--- git-trello-hook.py
+++ git-trello-hook.py
@@ -43,7 +43,7 @@ def handle_payload():
     commits = json_payload['commits']
     cards_in_commit = []
     cards_url_dict = {}
-    card_pattern = '(card #)([0-9]+)'
+    card_pattern = '(\[)(card #)([0-9]+)(\])'
 
     for commit in commits:
         results = re.findall(
@@ -66,6 +66,19 @@ def handle_payload():
 
                 TRELLO_CARDS.update(
                     card['id'], desc=desc_with_commit, idList=TRELLO_CONFIG['list_id_done'])
+
+    else:
+        from_cards = TRELLO_LIST.get_card(
+                    TRELLO_CONFIG['list_id_to_do'])
+
+                for card in from_cards:
+                    print(card)
+                    if str(card['idShort']) in cards_in_commit:
+                        desc_with_commit = '{0}\n{1}'.format(
+                            card['desc'], cards_url_dict[str(card['idShort'])])
+
+                        TRELLO_CARDS.update(
+                            card['id'], desc=desc_with_commit, idList=TRELLO_CONFIG['list_id_in_progress'])
 
     return "done"
 
